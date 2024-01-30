3b58e602e9ec6da0a4230a9dddc6285e78bd0294
Silverdragon977 <55039285+Silverdragon977@users.noreply.github.com>
2024-01-29 19:57:05 -0600

    Create footer.view.php

---
+++ src/BoilerPlate/footer.view.php
@@ -0,0 +1,9 @@
+<br><br><br><br><br><br><br><br>
+    <footer>
+        <center>
+            <h6><a href="https://google.com"><i> GitHub Repository Code <i></a> <h6>
+        </center>
+    </footer>
+</div>
+</body>
+</html>
