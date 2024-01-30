97e75db0dfe95342265ab797695a7f3d3cd650b5
Silverdragon977 <55039285+Silverdragon977@users.noreply.github.com>
2024-01-29 20:31:03 -0600

    Update header.view.php

--- src/BoilerPlate/header.view.php
+++ src/BoilerPlate/header.view.php
@@ -6,7 +6,7 @@
     <header id="banner-container">
         <div id="overlay-div">
             <img id="profileImage" src="src/media/bankIcon.jpg">
-            <h1 class ="header-heading">&nbsp;&nbsp;&nbsp;&nbsp;<?php echo "Hello $user" ?></h1>
+            <h1 class ="header-heading">&nbsp;&nbsp;&nbsp;&nbsp;Bank Name</h1>
         </div>
 
         <!--<img id="banner" src="src/media/banner.jpg" alt="Banner Image">-->
