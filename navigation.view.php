a7a593c74220c89e0251e5abd73f352b88e780f3
Harrymullabu <hmullabu@gmail.com>
2024-01-30 16:20:59 -0600

    navbar

--- navigation.view.php
+++ navigation.view.php
@@ -1,14 +1,13 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Bank System</title>
-</head>
+
 <body>
     <h2>Welcome to the Bank System</h2>
     <ul>
-        <li><a href="view_transactions.php">View Transactions</a></li>
-        <li><a href="make_transaction.php">Make a Transaction</a></li>
+        <li><a href='account.php'>Account</a></li>
+        <li><a href='transfers.php'>Transfers</a></li>
+        
         
     </ul>
 </body>
