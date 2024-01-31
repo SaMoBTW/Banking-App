b1f188ac75bc34f39ef7425b7c4359a02c47b2a2
Echamma <151067103+Echamma@users.noreply.github.com>
2024-01-31 13:05:37 -0600

    Checking dev

--- src/transaction.php
+++ src/transaction.php
@@ -11,26 +11,7 @@ define('URL_BASE', $config[$environment]["URL_BASE"]);
 include_once(APP_ROOT. "/data.php");
 include_once(APP_ROOT . "/BoilerPlate/head.view.php");
 include_once(APP_ROOT . "/BoilerPlate/header.view.php");
-function updateCheckingDetails($data) {
-    // Ensure session is started
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
 
-    foreach ($data as $key => $check) {
-        $sessionKey = $check['name'] . 'amount';
-
-        if (isset($_SESSION[$sessionKey])) {
-            // Update array with session data
-            $data[$key]['amount'] = $_SESSION[$sessionKey];
-        } else {
-            // Initialize session data with array value
-            $_SESSION[$sessionKey] = $check['amount'];
-        }
-    }
-
-    return $data; // Return the updated array
-}
 foreach ($data as $check) {
     if (isset($_POST[$check['name']]))
     {
@@ -44,7 +25,8 @@ foreach ($data as $check) {
 }
 
 ?>
-<form method="post">
+
+<form method="post" action="">
 From: 
   <select name="from" id="from">
     <option value="Checking 1" name = "Checking 1">Checking1</option>
