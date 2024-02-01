d78118b8b62905a6a795b784a5d971f55604f521
Samir Mahmoud <84868862+SaMoBTW@users.noreply.github.com>
2024-01-31 19:14:02 -0800

    nav bar

--- src/BoilerPlate/navigation.view.php
+++ src/BoilerPlate/navigation.view.php
@@ -1,14 +1,1 @@
 
-<!DOCTYPE html>
-<html>
-
-<body>
-    <h2>Welcome to the Bank System</h2>
-    <ul>
-        <li><a href='account.php'>Account</a></li>
-        <li><a href='transfers.php'>Transfers</a></li>
-        
-        
-    </ul>
-</body>
-</html>
