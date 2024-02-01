93a8818783267b5fd473f235fba8bc49a6bb795d
Samir Mahmoud <84868862+SaMoBTW@users.noreply.github.com>
2024-01-31 17:53:01 -0800

    ,kjhg

--- src/transaction.php
+++ src/transaction.php
@@ -26,25 +26,38 @@ foreach ($data as $check) {
 }
 
 ?>
-<form method="post">
-From: 
-  <select name="from" id="from">
-    <option value="Checking 1" name = "Checking 1">Checking1</option>
-    <option value="Checking 2" name = "Checking 2">Checking2</option>
-    <option value="Checking 3" name = "Checking 3">Checking3</option>
-  </select><br>
-To: 
-  <select name="To" id="from">
-    <option value="Checking 1" name = "Checking 1">Checking1</option>
-    <option value="Checking 2" name = "Checking 2">Checking2</option>
-    <option value="Checking 3" name = "Checking 3">Checking3</option>
+<form class="transaction-form" method="post">
 
-  </select><br>
-Amount: 
+  <h2>
+    From:
+  </h2> 
+  <select name="from" id="from">
+    <option value="checking1" name = "Checking 1">Checking1</option>
+    <option value="checking2" name = "Checking 2">Checking2</option>
+    <option value="checking3" name = "Checking 3">Checking3</option>
+  </select>
+  <input type="text" type='textbox' class="trans-acc-balance">
+  <br>
+
+  <h2>
+    To: 
+  </h2>
+  <select name="from" id="from">
+    <option value="checking1" name = "Checking 1">Checking1</option>
+    <option value="checking2" name = "Checking 2">Checking2</option>
+    <option value="checking3" name = "Checking 3">Checking3</option>
+  </select>
+  <input type="text" type='textbox' class="trans-acc-balance">
+  <br>
+
+  <h2>
+    Amount:
+  </h2> 
+
   <input type="text" name="amount" >
   <br><br>
-  <input type="submit">
-
+  <input type="submit" class="transaction-form-button">
+  
 </form>
 
 <?php
