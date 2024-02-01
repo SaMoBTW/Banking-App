3f3970202b546f8fe58cab75660053d74c1c4f50
Samir Mahmoud <84868862+SaMoBTW@users.noreply.github.com>
2024-01-31 20:17:48 -0800

    fixed nav bar

--- src/BoilerPlate/header.view.php
+++ src/BoilerPlate/header.view.php
@@ -11,12 +11,12 @@ $user = $_SESSION['$user'];
             <img id="profileImage" src="./media/bankIcon.jpg">
             <h1 class ="header-heading">&nbsp;&nbsp;&nbsp;&nbsp; <?php echo "Welcome " . $user ?></h1>
 
-                <nav class="nav-bar">
-        <ul>
-            <li class='nav-bar-listitems'><a href='/src/transaction.php'>Transfers</a></li>
-            <li class="nav-bar-listitems"><a href="/src/account.php">Account</a></li>
-        </ul>
-    </nav>
+            <nav class="nav-bar">
+                <ul>
+                    <li class='nav-bar-buttons'><a href='/src/transaction.php'>Transfers</a></li>
+                    <li class="nav-bar-buttons"><a href="/src/account.php">Accounts</a></li>
+                </ul>
+            </nav>
         </div>
 
         <!--<img id="banner" src="src/media/banner.jpg" alt="Banner Image">-->
